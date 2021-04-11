76be526b9202d8d5e0a15da0e64a53e4aab9f96b
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-11 12:06:25 -0700

    feat: delete card finish-33

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import faker from "faker"
 function App() {
   const [cards, setCards] = useState([
     {
+      id: 'ajsldh',
       name: 'Osbaldo Hodkiewicz IV',
       title: 'CEO',
       avatar: 'https://cdn.fakercloud.com/avatars/her_ruu_128.jpg'
     },
     {
+      id: 'iekdhr',
       name: 'Eldora Rice',
       title: 'designer',
       avatar: 'https://cdn.fakercloud.com/avatars/umurgdk_128.jpg'
     },
     {
+      id: 'hslfhf',
       name: 'Kane Donnelly',
       title: 'Web Developer',
       avatar: 'https://cdn.fakercloud.com/avatars/mastermindesign_128.jpg'
@@ -28,6 +31,8 @@ function App() {
   const deleteCardHandler = (cardIndex) => {
     const cards_copy = [...cards]
     cards_copy.splice(cardIndex, 1)
+    console.log("cards", cards)
+    console.log("copay cards",cards_copy)
     setCards(cards_copy)
   }
   const cardsMarkup = showCard &&
@@ -36,6 +41,7 @@ function App() {
         avatar={card.avatar}
         name={card.name}
         title={card.title}
+        key={card.id}
         onDelete={() => deleteCardHandler(index)}
       />)
     )
