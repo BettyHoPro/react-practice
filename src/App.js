1edba0d49ad44bfb5d8709f80eb8ec821067471d
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-11 11:28:29 -0700

    refactory: cardsMarkup and btnsMarkup:

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ import faker from "faker"
 function App() {
   const [ name, setName] = useState("Betty Ho")
   const [showCard, setShowCard] = useState(true)
+  
+  const changeNameHandler = (name) => setName(name)
+  const changeInputHandler = event => setName(event.target.value)
+  const toggleShowCard = () => setShowCard(!showCard)
   const btnsMarkup = (
     <div>
     <button className="button button2">Yes</button>
     <button className="button button3">No</button>
     </div>
   )
-  const changeNameHandler = (name) => setName(name)
-  const changeInputHandler = event => setName(event.target.value)
-  const toggleShowCard = () => setShowCard(!showCard)
+  const cardsMarkup = showCard &&
+    <Card 
+       avatar="https://cdn.fakercloud.com/avatars/antonyzotov_128.jpg"
+       name={name}
+       title="Future Group Executive"
+       onChangeName={() => changeNameHandler("betty Ho")}
+       onChangeInput={changeInputHandler}
+     >{btnsMarkup}
+     </Card>
+
   return (
     <div className="App">
       <button className="button" onClick={toggleShowCard}> Toggle show/hide</button>
-      {showCard &&
-     <Card 
-        avatar="https://cdn.fakercloud.com/avatars/antonyzotov_128.jpg"
-        name={name}
-        title="Future Group Executive"
-        onChangeName={() => changeNameHandler("betty Ho")}
-        onChangeInput={changeInputHandler}
-      >{btnsMarkup}
-      </Card>}
-      
+      {cardsMarkup}
     </div>
   
   )
