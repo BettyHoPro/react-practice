04db398eab74e08bb2efc4ef1435b14e8dc51e88
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-11 11:30:54 -0700

    update: clean out the practice no use codes

--- src/App.js
+++ src/App.js
@@ -7,24 +7,14 @@ import faker from "faker"
 function App() {
   const [ name, setName] = useState("Betty Ho")
   const [showCard, setShowCard] = useState(true)
-  
-  const changeNameHandler = (name) => setName(name)
-  const changeInputHandler = event => setName(event.target.value)
+
   const toggleShowCard = () => setShowCard(!showCard)
-  const btnsMarkup = (
-    <div>
-    <button className="button button2">Yes</button>
-    <button className="button button3">No</button>
-    </div>
-  )
   const cardsMarkup = showCard &&
     <Card 
        avatar="https://cdn.fakercloud.com/avatars/antonyzotov_128.jpg"
        name={name}
        title="Future Group Executive"
-       onChangeName={() => changeNameHandler("betty Ho")}
-       onChangeInput={changeInputHandler}
-     >{btnsMarkup}
+     >
      </Card>
 
   return (
