219c932859020965dde990975495ee12eb2cc268
BettyHoPro <bettyvisualtech@gmail.com>
2021-05-01 14:31:39 -0700

    feat: convert function base to class base

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react"
+import React, { Component } from "react"
 import './App.css';
 import Card from "./Card"
 import { ThemeProvider } from "styled-components"
@@ -9,70 +9,79 @@ const theme = {
   mango: "yellow" 
 }
 
-function App() {
-  const [cards, setCards] = useState([
-    {
-      id: 'ajsldh',
-      name: 'Osbaldo Hodkiewicz IV',
-      title: 'CEO',
-      avatar: 'https://cdn.fakercloud.com/avatars/her_ruu_128.jpg'
-    },
-    {
-      id: 'iekdhr',
-      name: 'Eldora Rice',
-      title: 'designer',
-      avatar: 'https://cdn.fakercloud.com/avatars/umurgdk_128.jpg'
-    },
-    {
-      id: 'hslfhf',
-      name: 'Kane Donnelly',
-      title: 'Web Developer',
-      avatar: 'https://cdn.fakercloud.com/avatars/mastermindesign_128.jpg'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards:[
+        {
+          id: 'ajsldh',
+          name: 'Osbaldo Hodkiewicz IV',
+          title: 'CEO',
+          avatar: 'https://cdn.fakercloud.com/avatars/her_ruu_128.jpg'
+        },
+        {
+          id: 'iekdhr',
+          name: 'Eldora Rice',
+          title: 'designer',
+          avatar: 'https://cdn.fakercloud.com/avatars/umurgdk_128.jpg'
+        },
+        {
+          id: 'hslfhf',
+          name: 'Kane Donnelly',
+          title: 'Web Developer',
+          avatar: 'https://cdn.fakercloud.com/avatars/mastermindesign_128.jpg'
+        }
+      ], 
+      showCard: true
     }
-  ])
-  const [showCard, setShowCard] = useState(true)
-
-  const toggleShowCard = () => setShowCard(!showCard)
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    console.log("cards", cards)
-    console.log("copay cards",cards_copy)
-    setCards(cards_copy)
   }
-  const changeNameHandler = (event,id) => {
-     const cardIndex = cards.findIndex(card => card.id === id)
-     const cards_copy = [...cards]
+  // const [cards, setCards] = useState()
+  // const [showCard, setShowCard] = useState(true)
+
+  toggleShowCard = () => this.setState({showCard: !this.state.showCard})
+  deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...this.state.cards]
+    cards_copy.splice(cardIndex, 1)
+    // console.log("cards", cards)
+    // console.log("copay cards",cards_copy)
+    this.setState({cards: cards_copy})
+  }
+  changeNameHandler = (event,id) => {
+     const cardIndex = this.state.cards.findIndex(card => card.id === id)
+     const cards_copy = [...this.state.cards]
      cards_copy[cardIndex].name = event.target.value
-     setCards(cards_copy)
+     this.setState({cards: cards_copy})
     }
 
-  const classes = ['button']
-  if (cards.length < 3) classes.push("pink")
-  if (cards.length < 2) classes.push('red text')
-
-  const cardsMarkup = showCard &&
-    (
-      cards.map((card, index) => <Card 
-        avatar={card.avatar}
-        name={card.name}
-        title={card.title}
-        key={card.id}
-        onDelete={() => deleteCardHandler(index)}
-        onChangeName = {(event) => changeNameHandler(event, card.id) }
-      />)
-    )
-   
-
+  render(){
+    const classes = ['button']
+    if (this.state.cards.length < 3) classes.push("pink")
+    if (this.state.cards.length < 2) classes.push('red text')
+  
+    const cardsMarkup = this.state.showCard &&
+      (
+        this.state.cards.map((card, index) => <Card 
+          avatar={card.avatar}
+          name={card.name}
+          title={card.title}
+          key={card.id}
+          onDelete={() => this.deleteCardHandler(index)}
+          onChangeName = {(event) => this.changeNameHandler(event, card.id) }
+        />)
+      )
+    
   return (
     <ThemeProvider theme={ theme}>
     <div className="App">
-      <Button color= "mango" length={cards.length}>Toggle</Button>
-      <button className={classes.join(' ')} onClick={toggleShowCard}> Toggle show/hide</button>
+      <Button color= "mango" length={this.state.cards.length} onClick={this.toggleShowCard}>Toggle</Button>
+      <button className={classes.join(' ')} onClick={this.toggleShowCard}> Toggle show/hide</button>
       {cardsMarkup}
     </div>
     </ThemeProvider>
   )
+  }
+
 }
 
 export default App;
